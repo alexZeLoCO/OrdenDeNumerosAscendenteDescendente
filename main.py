a3e19a3b6f956e5743cbc98c541fc28b96577ea4
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-15 15:52:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n1 = float (input ("Introduzca un número: "))
+n2 = float (input ("Introduzca un número: "))
+n3 = float (input ("INtroduzca un número: "))
+if (n1<n2<n3):
+    print ("Los números",n1,n2,n3,"están en orden ascendente.")
+elif (n1>n2>n3):
+    print ("Los números",n1,n2,n3,"están en orden descendente.")
+else:
+    print ("Los números",n1,n2,n3,"no presentan ningún orden.")
